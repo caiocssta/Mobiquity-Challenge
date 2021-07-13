beab8abfd86e17ec5f72dcfa68ba8aa11bb33d7e
Caio Costa <caio@adopets.org>
2021-07-12 22:05:29 -0300

    Command creation to validate email

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,3 +15,9 @@ Cypress.Commands.add('getCommentsByPost', (postId) => {
         return(response.body)
     })
 })
+
+Cypress.Commands.add('validateEmail', (email) => {
+    console.log('validating', email)
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+})
